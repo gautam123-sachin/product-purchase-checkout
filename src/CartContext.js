8aef4fb5d96c081228d3b45af53fa37a2f5eee6b
sachin <sachingautam6239@gmail.com>
2023-11-09 10:48:39 +0530

    New ui update

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,11 +1,17 @@
 import React, { createContext, useContext, useReducer } from "react";
 
+// Create a context for the cart
 const CartContext = createContext();
+
+// Create a context for the customer info
 const CustomerInfoContext = createContext();
 
+// Custom hook to access the cart context
 export function useCart() {
   return useContext(CartContext);
 }
+
+// Reducer function for the cart state
 function cartReducer(state, action) {
   switch (action.type) {
     case "ADD_TO_CART":
@@ -17,8 +23,10 @@ function cartReducer(state, action) {
   }
 }
 
+// Provider component for the cart context
 export function CartProvider({ children }) {
   const [cart, dispatch] = useReducer(cartReducer, []);
+
   return (
     <CartContext.Provider value={{ cart, dispatch }}>
       {children}
@@ -26,21 +34,24 @@ export function CartProvider({ children }) {
   );
 }
 
+// Custom hook to access the customer info context
 export function useCustomerInfo() {
   return useContext(CustomerInfoContext);
 }
 
+// Reducer function for the customer info state
 function customerInfoReducer(state, action) {
   switch (action.type) {
     case "SET_CUSTOMER_INFO":
       return { ...state, ...action.payload };
     case "CLEAR_CUSTOMER_INFO":
-      return [];
+      return { firstName: "", lastName: "", email: "", phone: "" }; // Clear the customer info by returning an empty object
     default:
       return state;
   }
 }
 
+// Provider component for the customer info context
 export function CustomerInfoProvider({ children }) {
   const [customerInfo, dispatch] = useReducer(customerInfoReducer, {
     firstName: "",
--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,84 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Link as MuiLink } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Link as MuiLink,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Hidden,
+} from "@mui/material";
 import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Header() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  // Define the AppBar title based on the mobile view
+  const appBarTitle = "product-purchase-checkout";
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
-          product-purchase-checkout
-        </Typography>
-        <nav>
-          <MuiLink
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Hidden mdUp>
+            <IconButton color="inherit" onClick={handleDrawerToggle}>
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
+
+          <Typography variant="h6" component="div" style={{ flexGrow: 1 }}>
+            {appBarTitle}
+          </Typography>
+
+          <Hidden smDown>
+            <MuiLink
+              component={Link}
+              to="/product-list"
+              color="inherit"
+              style={{ marginRight: 20, textDecoration: "none" }}
+            >
+              Product List
+            </MuiLink>
+            <MuiLink
+              component={Link}
+              to="/checkout"
+              color="inherit"
+              style={{ marginRight: 20, textDecoration: "none" }}
+            >
+              Checkout Page
+            </MuiLink>
+          </Hidden>
+        </Toolbar>
+      </AppBar>
+
+      {/* Side menu for mobile view */}
+      <Drawer open={drawerOpen} onClose={handleDrawerToggle}>
+        <List>
+          <ListItem
             component={Link}
             to="/product-list"
-            color="inherit"
-            style={{ marginRight: 20,  textDecoration: "none" }}
+            onClick={handleDrawerToggle}
           >
-            Product List
-          </MuiLink>
-          <MuiLink
+            <ListItemText primary="Product List" />
+          </ListItem>
+          <ListItem
             component={Link}
             to="/checkout"
-            color="inherit"
-            style={{ marginRight: 20,  textDecoration: "none" }}
+            onClick={handleDrawerToggle}
           >
-            Checkout Page
-          </MuiLink>
-        </nav>
-      </Toolbar>
-    </AppBar>
+            <ListItemText primary="Checkout Page" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
   );
 }
 
